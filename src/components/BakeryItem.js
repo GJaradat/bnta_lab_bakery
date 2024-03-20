cc9b9f741d8a02788c05ff0b52c358526711b937
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-03-20 22:02:00 +0000

    Changed rating system to stars

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,16 +1,26 @@
 import './BakeryItem.css';
 
+
 const BakeryItem = ({cake}) => {
 
-    //
+    const ratingHandler = (rating) => {
+        let starRating = ""
+        for (let i = 0; i < rating; i++) { 
+            starRating += "⭐" 
+        } return starRating;
+    }
+
+    const starRating = ratingHandler(cake.rating);
 
     return (
         <section className="bakery-item">
-            <div>
+            {/* Why was it not detecting the image when i just selected src = "./placeholder_cake.jpg" */}
+            <img src = "https://img.freepik.com/free-vector/cute-vector-illustration-delicious-dessert_25030-68252.jpg?w=996&t=st=1710970942~exp=1710971542~hmac=0ccdf06fc3cf1a69d7763a343abf5e2d807d603c7fe072a61622b93feebf59a8 "alt={cake.cakeName} />
+            <div className="bakery-item-header">
                 <h3>{cake.cakeName}</h3>
                 <h3>£{cake.price}</h3>
             </div>
-                <h3>{cake.rating}</h3>
+                <h3>{starRating}</h3>
             <div>
                 <button>Buy</button>
                 <button>List ingredients</button>
